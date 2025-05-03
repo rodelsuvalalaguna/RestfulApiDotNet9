ad488ffea18247b8708a13723035d56f36d0befe
rodelsuvalalaguna <rodel.s.lalaguna@gmail.com>
2025-05-03 21:22:21 +0800

    Added swagger

--- RestfulApi.Api/Program.cs
+++ RestfulApi.Api/Program.cs
@@ -16,8 +16,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddAutoMapper(typeof(UserProfile).Assembly);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{ 
+    app.UseSwagger();   
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();  
 app.MapControllers();   
